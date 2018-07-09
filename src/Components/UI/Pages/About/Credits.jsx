978efebacbe33ddcc38b21e7b719839baec547af
Anatoliy Zaslavskiy <adz@nycitt.com>
2018-07-09 05:32:22 -0700

    About us fixed

--- src/Components/UI/Pages/About/Credits.jsx
+++ src/Components/UI/Pages/About/Credits.jsx
@@ -1,100 +1,77 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
 import {
   Desktop,
   PhoneLandscape,
   PhonePortrait,
   Orange,
-} from "../../DeviceRules";
+} from '../../DeviceRules';
 
-export default ({ person: {
- name, img, text, profileSite 
-} }) => (
+const Credit = styled.div`
+  display: flex;
+
+  padding: 10px;
+`;
+
+const Image = styled.img`
+  @media ${Desktop} {
+    height: 100%;
+    width: 100px;
+    border-radius: 5px;
+  }
+  @media ${PhonePortrait}{
+    width: 40vw;
+    height: auto;
+    margin-top: 5vh;
+  }
+  @media ${PhoneLandscape}{
+    height: 100%;
+    width: 158px;
+  }
+`;
+
+const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 0 10px;
+`;
+
+const CreditText = styled.p`
+  font-family: Roboto Condensed;
+`;
+
+const Title = styled.h2`
+  color: ${Orange};
+`;
+
+const Credits = ({
+  person: {
+    name, img, text, profileSite,
+  },
+}) => (
   <Credit>
     <Image src={img} alt="loading..." />
-    <Text_Box>
-      <Title>
-        <Link href={profileSite}>
-{name}
-</Link>
+    <TextBox>
+      <Title href={profileSite}>
+        {name}
       </Title>
-      <Credit_Text>
-{text}
-</Credit_Text>
-    </Text_Box>
+      <CreditText>
+        {text}
+      </CreditText>
+    </TextBox>
   </Credit>
 );
 
+Credits.propTypes = {
+  person: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    profileSite: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
-const Credit = styled.div`
-  display: grid;
-  height: 20vh;
-  width: 70vw;
-  grid-template-columns: 1fr auto 1fr 6fr;
-  grid-template-areas: ". img . textbox";
-  margin: 2vh 0;
-  @media ${PhonePortrait}{
-    width: 100%;
-    display:flex;
-    height:25vh;
-    margin: 0 0 6vh 0;
-    padding: 0 5px;
-  }
-  @media ${PhoneLandscape}{
-    display: flex;
-    height:80%;
-    margin: 0 0 1vh 0;
-  }
-
-`;
-const Image = styled.img`
-@media ${Desktop}{
-      grid-area: img
-      height: 100%
-      border-radius: 5px
-    }
-    @media ${PhonePortrait}{
-      width: 40vw
-      height: auto
-      margin-top: 5vh
-    }
-    @media ${PhoneLandscape}{
-      height: 100%;
-      width: 158px;
-    }
-`;
-const Text_Box = styled.div`
-  grid-area: textbox;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  padding: 1vh;
-  @media ${PhonePortrait}{
-    margin-left: 20px;
-  }
-  @media ${PhoneLandscape}{
-    margin-left: 8vw;
-  }
-`;
-const Credit_Text = styled.p`
-  grid-area: text;
-  flex-grow: 1;
-  margin: 3vh 0;
-  font-size: 1.5vh;
-  font-family: Roboto Condensed;
-  @media ${PhonePortrait}{
-    margin: 1vh 0;
-    font-size: 0.8rem;
-  }
-  @media ${PhoneLandscape}{
-    font-size:3vh;
-  }
-`;
-const Link = styled.a`
-  color: ${Orange};
-`;
-const Title = styled.h2`
-  @media ${PhonePortrait}{
-    font-size: 1.5rem;
-  }
-`;
+export default Credits;
--- src/Components/UI/Pages/About/index.jsx
+++ src/Components/UI/Pages/About/index.jsx
@@ -11,12 +11,18 @@ const team = [{
   profileSite: 'https://yardenh.herokuapp.com/',
   text:
     'Yarden is a Full Stack Engineer currently looking for opportunities in Colorado or California. His favorite fruit is Mango.',
+}, {
+  name: 'Anatoliy Zaslavskiy',
+  img: 'https://res.cloudinary.com/dz2nxhscn/image/upload/v1518127206/20046539_10156211418873265_263186605151505675_n_ah72ly.jpg',
+  profileSite: 'https://tolicodes.com',
+  text: 'Toli is a frontend programmer working at Hover Inc in SF, California. He loves everything active including acro yoga, hiking, jogging, skiing, and PsyTrance parties.',
 }];
 
 const AboutPage = styled.div`
   display: flex;
   flex-direction: column;
   height: 80vh;
+  font-size: 14px;
 
   @media ${PhoneLandscape}{
     height: 100vh;
